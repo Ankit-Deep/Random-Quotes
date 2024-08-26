9337d52bbf6ad58367b1d9be5e73dfe8f4622349
Ankit Deep <AnkitDeep01@outlook.com>
2024-08-26 22:54:53 +0530

    Add all files

---
+++ script.js
@@ -0,0 +1,18 @@
+const quote = document.querySelector('.quote');
+const author = document.querySelector('.author');
+const button = document.querySelector('button');
+
+const api = 'https://api.quotable.io/quotes/random';
+
+const getQuote = () => {
+    fetch('https://quotes-api-self.vercel.app/quote')
+    .then((data) => data.json())
+    .then((item) => {
+        console.log(item);
+        quote.innerHTML = item.quote;
+        author.innerHTML = `~${item.author}`;
+    })
+}
+
+window.addEventListener("load", getQuote);
+button.addEventListener('click', getQuote);
